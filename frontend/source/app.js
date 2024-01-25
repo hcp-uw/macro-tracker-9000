3e5dbb573706ec78782e87da65d59ed3ab71d2e6
Christopher Chen <115045725+cmchen22@users.noreply.github.com>
2024-01-25 15:24:42 -0800

    new

--- frontend/source/app.js
+++ frontend/source/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import HomePage from './screens/Homepage';          // Import HomePage
 import CameraScreen from './screens/CameraScreen';
 import MealHistoryScreen from './screens/MealHistoryScreen';
 import SettingsScreen from './screens/SettingsScreen';
@@ -9,7 +10,8 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={CameraScreen} />
+        <Route path="/" exact component={HomePage} />   // Set HomePage as the root route
+        <Route path="/camera" component={CameraScreen} />
         <Route path="/meal-history" component={MealHistoryScreen} />
         <Route path="/settings" component={SettingsScreen} />
         {/* You can add more routes here */}
