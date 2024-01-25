317bce67d3cd40ca24d95179daa46069bea3b350
Christopher Chen <115045725+cmchen22@users.noreply.github.com>
2024-01-25 15:06:49 -0800

    commit strucutre

--- frontend/source/app.js
+++ frontend/source/app.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import CameraScreen from './screens/CameraScreen';
+import MealHistoryScreen from './screens/MealHistoryScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={CameraScreen} />
+        <Route path="/meal-history" component={MealHistoryScreen} />
+        <Route path="/settings" component={SettingsScreen} />
+        {/* You can add more routes here */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
