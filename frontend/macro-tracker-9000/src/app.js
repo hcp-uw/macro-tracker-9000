2080ce1f1c8c728b1ed819028f902102c5590e29
Christopher Chen <115045725+cmchen22@users.noreply.github.com>
2024-01-25 16:00:56 -0800

    changed

--- frontend/macro-tracker-9000/src/app.js
+++ frontend/macro-tracker-9000/src/app.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import HomePage from './screens/HomepagemePage
-import CameraScreen from './screens/CameraScreen
-import MealHistoryScreen from './screens/MealHistoryScreen
-import SettingsScreen from './screens/SettingsScreen
+import HomePage from './screens/HomePage'; // Correct the file path if necessary
+import CameraScreen from './screens/CameraScreen'; // Add the missing semicolon at the end
+import MealHistoryScreen from './screens/MealHistoryScreen'; // Add the missing semicolon at the end
+import SettingsScreen from './screens/SettingsScreen'; // Add the missing semicolon at the end
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Homepage} />
+        <Route path="/" exact component={Homepage} /> {/* Use HomePage, the name you imported */}
         <Route path="/camera" component={CameraScreen} />
         <Route path="/meal-history" component={MealHistoryScreen} />
         <Route path="/settings" component={SettingsScreen} />
