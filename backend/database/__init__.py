c976305788630481988e889fc869800f9bcc9ee6
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2024-02-20 19:33:29 -0800

    finish db creation

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-load_dotenv()
 
 host = os.getenv("DATABASE_HOST")
 user = os.getenv("DATABASE_USERNAME")
@@ -17,5 +16,3 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from sqlalchemy import select
 from database.models import User, Meal
 from database.database import engine, Base
 
-from .database import crud, models, schemas, SessionLocal, engine
+from database import crud, models, schemas, SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -27,17 +27,25 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Username already registered")
     return crud.create_user(db=db, user=user)
 
-@app.get("/users", response_model=list[schemas.User])
+@app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
-@app.get
+@app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
-
+@app.post("/users/{user_id}/meals/", response_model=schemas.Meal)
 def create_meal_for_user(
     user_id: int, meal: schemas.MealCreate, db: Session = Depends(get_db)
 ):
+    return crud.create_user_meal(db=db, meal=meal, user_id=user_id)
     
+@app.get("/meals/", response_model=list[schemas.Meal])
 def read_meals(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    meals = crud.get_meals(db, skip=skip, limit=limit)
+    return meals
