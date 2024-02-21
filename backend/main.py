7163807834bdf1f2a967b88f983bfa8a08453a3c
Keanu Thakalath <kthakala@uw.edu>
2024-02-20 18:54:36 -0800

    made index create tables

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, Path
 from typing import Optional
 from pydantic import BaseModel
+from sqlalchemy import select
+from database.models import User, Meal
+from database.database import engine, Base
 
 app = FastAPI()
 
 @app.get("/")
 def index():
+    with engine.connect() as session:
+        Base.metadata.create_all(engine)
+        return session.scalars(select(User)).all()
     return {"matt the pm"}
