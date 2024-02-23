6ba6b523f7205fe8724d0047a6c0e7c2a48266a9
Keanu Thakalath <kthakala@uw.edu>
2024-02-22 17:15:08 -0800

    fixed duplicate code in __init__.py

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,18 +1,1 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
-
-
-host = os.getenv("DATABASE_HOST")
-user = os.getenv("DATABASE_USERNAME")
-passwd = os.getenv("DATABASE_PASSWORD")
-db = os.getenv("DATABASE")
-
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{user}:{passwd}@{host}/{db}"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from .database import engine, SessionLocal
