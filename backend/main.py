f3b856000b422f96a7b442fb48c3bd39a535fbd6
santosmatt117 <81929979+santosmatt117@users.noreply.github.com>
2024-04-23 18:47:28 -0700

    ayayyy

--- backend/main.py
+++ backend/main.py
@@ -78,7 +78,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     if not user:
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
-            deetail="Incorrect username or password",
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
